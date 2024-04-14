a30a06ba0be84c138db6c819762b3f0bc1a7177c
mascarponeee <lelchiiik@gmail.com>
2024-04-14 23:36:20 +0300

    added routing to item's page

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,19 +1,20 @@
 import "../styles/item.css"
+import { Link } from 'react-router-dom'
 
 function Item({item}) {
   return (
     <div className="item">
       <div className="item__img">
-        <a href="">
-        <img src={require("../assets/catalog/truck_1.jpg")} alt="Фотография погрузчика" />
-        </a>
+        <Link to={`/catalog/${item._id}`}>
+        <img src={require("../assets/catalog/images/lza-40810-kh3-3.jpg")} alt="Фотография погрузчика" />
+        </Link>
       </div>
-      <a href="" className="item__name">{item.name}</a>
+      <Link to={`/catalog/${item._id}`} className="item__name">{item.name}</Link>
       <div className="item__description">
         <p>Грузоподъемность (кг): <span>{item.liftingCapacity}</span></p>
         <p>Высота подъема (м): <span>{item.liftingHeight[0]}</span></p>
       </div>
-      <a href="" className="item__more">Подробнее</a>
+      <Link to={`/catalog/${item._id}`} className="item__more">Подробнее</Link>
     </div>
   );
 }
